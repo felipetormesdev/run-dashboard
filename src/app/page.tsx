745a058373192a055f00ac194c6d1ab49f56cd21
Felipe Tormes <felipe.tormes.dev@gmail.com>
2025-09-08 10:33:44 -0300

    chore: init Next.js project and create initial project structure (components, lib, dashboard)

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+// src/app/dashboard/page.tsx
+export default function DashboardPage() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-white">
+      <h1 className="text-3xl font-bold text-green-600">
+        Dashboard inicial 🚀
+      </h1>
+    </main>
+  )
+}
